ac295da5843cf200a69157f4e7e05e2836659672
ZoxDev <mathias.bar1809@gmail.com>
2024-04-09 17:47:40 +0200

    feat: map all post

--- jsonplaceholderlanding/src/views/LandingPage.tsx
+++ jsonplaceholderlanding/src/views/LandingPage.tsx
@@ -1,25 +1,25 @@
 import PostCard from '../components/PostCard';
-import Comment from '../components/Comment';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+import usePosts from '../hooks/usePosts';
 
 const LandingPage = () => {
+  const postList = usePosts();
+
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <main className="w-scree h-screen bg-gray-800 text-white">
-          <section className="w-screen h-2/6 flex flex-col gap-11 justify-center items-center bg-gray-900">
-            <h1 className="text-4xl">Welcome to JSONPlaceHolder Posts !</h1>
-            <p className="text-xl">
-              Here you can visit post below and click on it to see there comments, insane huh 😊
-            </p>
-          </section>
-
-          <PostCard></PostCard>
-          <Comment></Comment>
-        </main>
-      </QueryClientProvider>
+      <main className="w-scree h-auto bg-gray-800 text-white">
+        <section className="w-screen h-56 flex flex-col gap-11 justify-center items-center bg-gray-900">
+          <h1 className="text-4xl">Welcome to JSONPlaceHolder Posts !</h1>
+          <p className="text-xl">
+            Here you can visit post below and click on it to see there comments, insane huh 😊
+          </p>
+        </section>
+        <section className="flex flex-wrap gap-4">
+          {postList.data?.map((post) => {
+            return <PostCard key={post.id} post={post}></PostCard>;
+          })}
+        </section>
+      </main>
     </>
   );
 };
