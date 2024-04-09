0f30c4ea2f2d53cc40b7ce2a6a355351f0514b82
ZoxDev <mathias.bar1809@gmail.com>
2024-04-09 18:00:34 +0200

    feat: add data to component

--- jsonplaceholderlanding/src/components/Comment.tsx
+++ jsonplaceholderlanding/src/components/Comment.tsx
@@ -1,13 +1,19 @@
-const Comment = () => {
+import { typeComment } from '../hooks/useComments';
+
+type CommentProps = {
+  comment: typeComment;
+};
+
+const Comment = ({ comment }: CommentProps) => {
   return (
     <>
       <section className="rounded-md bg-gray-700 border-2 w-1/6">
         <div className="p-5 flex flex-col gap-6">
           <div className="flex gap-4 ">
-            <h2 className="text-3xl font-bold">Name</h2>
-            <span className="text-base font-light">Email</span>
+            <h2 className="text-3xl font-bold">{comment.name}</h2>
+            <span className="text-base font-light">{comment.email}</span>
           </div>
-          <p className="text-xl">"text of the comment"</p>
+          <p className="text-xl">" {comment.body} "</p>
         </div>
       </section>
     </>
