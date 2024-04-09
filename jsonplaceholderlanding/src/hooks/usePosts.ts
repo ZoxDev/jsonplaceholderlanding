dfcd9a4f242143af941a9d771b88846e908b8331
ZoxDev <mathias.bar1809@gmail.com>
2024-04-09 17:06:25 +0200

    feat: create hook usePost

---
+++ jsonplaceholderlanding/src/hooks/usePosts.ts
@@ -0,0 +1,25 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+
+type typePost = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const usePosts = () => {
+  return useQuery({
+    queryKey: ['getTagInPage'],
+    queryFn: async () => {
+      const data: typePost = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) =>
+        res.json(),
+      );
+
+      return data;
+    },
+    placeholderData: keepPreviousData,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export default usePosts;
