d108d0d3095c0505f2814a588db7c2b227c5df9c
ZoxDev <mathias.bar1809@gmail.com>
2024-04-09 17:45:06 +0200

    feat: add data to post component

--- jsonplaceholderlanding/src/components/PostCard.tsx
+++ jsonplaceholderlanding/src/components/PostCard.tsx
@@ -1,10 +1,16 @@
-const PostCard = () => {
+import { typePost } from '../hooks/usePosts';
+
+type PostCardProps = {
+  post: typePost;
+};
+
+const PostCard = ({ post }: PostCardProps) => {
   return (
     <>
       <section className="rounded-md bg-gray-700 border-2 w-1/6 h-1/4">
         <div className="p-5 flex flex-col gap-5 ">
-          <h2 className="text-xl font-bold">Title</h2>
-          <div>body of the card</div>
+          <h2 className="text-xl font-bold">{post.title}</h2>
+          <div>{post.body}</div>
         </div>
       </section>
     </>
