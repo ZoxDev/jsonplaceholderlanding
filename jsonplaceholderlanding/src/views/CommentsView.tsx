96019c4f64631121b517b1e1def84992882275cc
ZoxDev <mathias.bar1809@gmail.com>
2024-04-09 18:07:55 +0200

    fix: style

--- jsonplaceholderlanding/src/views/CommentsView.tsx
+++ jsonplaceholderlanding/src/views/CommentsView.tsx
@@ -11,8 +11,8 @@ const CommentView = () => {
 
   return (
     <>
-      <main className="w-scree h-screen bg-gray-800 text-white">
-        <section className="w-screen h-2/6 flex flex-col gap-11 justify-center items-center bg-gray-900">
+      <main className="w-scree h-auto bg-gray-800 text-white">
+        <section className="w-screen h-56 flex flex-col gap-11 justify-center items-center bg-gray-900">
           <h1 className="text-4xl">Title of the post</h1>
           <p className="text-xl">Body of the post</p>
         </section>
