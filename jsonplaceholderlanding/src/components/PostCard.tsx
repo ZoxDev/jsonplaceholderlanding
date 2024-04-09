ea047144c206bfde2d2f2d2e45360c1228d76762
ZoxDev <mathias.bar1809@gmail.com>
2024-04-09 17:26:02 +0200

    style: fix flex gap on post

--- jsonplaceholderlanding/src/components/PostCard.tsx
+++ jsonplaceholderlanding/src/components/PostCard.tsx
@@ -1,8 +1,8 @@
 const PostCard = () => {
   return (
     <>
-      <section className="flex flex-col gap-5 rounded-md bg-gray-700 border-2 w-1/6 h-1/4">
-        <div className="p-5">
+      <section className="rounded-md bg-gray-700 border-2 w-1/6 h-1/4">
+        <div className="p-5 flex flex-col gap-5 ">
           <h2 className="text-xl font-bold">Title</h2>
           <div>body of the card</div>
         </div>
